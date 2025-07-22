9a33789aeaa1081ef704c9203ab71a240188ead9
rof1yev <rofiyevdilshod@gmail.com>
2025-07-22 17:36:45 +0500

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="min-h-screen fle">
+      <h1>
+        Home <sup>(01)</sup>
+      </h1>
+    </div>
+  );
+};
+
+export default HomePage;
